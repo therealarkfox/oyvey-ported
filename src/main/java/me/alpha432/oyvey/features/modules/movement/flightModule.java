27d112cbc152b3fba5ddb38f75f98cf8dffa3333
therealarkfox <thereal.arkfox@gmail.com>
2026-07-27 21:22:35 -0400

    flightModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/movement/flightModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/flightModule.java
@@ -5,10 +5,10 @@ import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 
-public class Flight extends Module {
+public class flightModule extends Module {
     private final Setting<Float> height = num("Height", 2f, 1f, 3f);
 
-    public Flight() {
+    public flightModule() {
         super("Flight", "flight..", Category.MOVEMENT);
     }
 
