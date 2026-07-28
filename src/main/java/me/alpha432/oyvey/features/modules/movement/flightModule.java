75cab9cf92ff20b2a6cd5086c6d4a7fa76ae3e9f
therealarkfox <thereal.arkfox@gmail.com>
2026-07-27 22:15:21 -0400

    flightModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/movement/flightModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/flightModule.java
@@ -5,10 +5,10 @@ import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 
-public class StepModule extends Module {
+public class flightModule extends Module {
     private final Setting<Float> height = num("Height", 2f, 1f, 3f);
 
-    public StepModule() {
+    public flightModule() {
         super("Step", "step..", Category.MOVEMENT);
     }
 
@@ -20,7 +20,7 @@ public class StepModule extends Module {
             float modifier = 1.0f;
             return;
         }
-        this.getAbilities().flying); {
+        this.getAbilities().flying {
           modifier *= 1.1F;
       }
     }
@@ -28,14 +28,8 @@ public class StepModule extends Module {
     @Override
     public void onDisable() {
         if (nullCheck()) return;
-        this.getAbilities().flying); {
+        this.getAbilities().flying {
           modifier = 1.0F;
       }
-    }
-
-    @Override
-    public void onTick() {
-        if (nullCheck()) return;
-        mc.player.getAttribute(Attributes.STEP_HEIGHT).setBaseValue(height.getValue());
     }
 }
